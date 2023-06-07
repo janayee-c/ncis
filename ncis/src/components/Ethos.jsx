a47a5edf77d57cb45d42430f68337007694ff729
Janaye Cheong <janaye.c13@gmail.com>
2023-06-07 11:55:32 -0700

    added main container

--- ncis/src/components/Ethos.jsx
+++ ncis/src/components/Ethos.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../styles/Ethos.css'
 import { Heading } from '../components/export'
+import { Container } from '@chakra-ui/react'
 
 const Ethos = () => {
   return (
     <div>
     <Heading title="ETHOS"></Heading>
-    
+    <Container id="main">
+      
+    </Container>
     </div>
   )
 }
