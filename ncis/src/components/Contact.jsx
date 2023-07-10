f5fb2ff510ca15dcef226a3809b09f803464ff56
Janaye Cheong <janaye.c13@gmail.com>
2023-07-10 14:38:54 -0600

    draft for presentation

--- ncis/src/components/Contact.jsx
+++ ncis/src/components/Contact.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import '../styles/Contact.css'
 import { Heading } from '../components/export'
-import { Container, Grid } from '@chakra-ui/react'
+import { Container, Grid, useMediaQuery } from '@chakra-ui/react'
 
 
 const Contact = () => {
+
+  const isMinimized = useMediaQuery('(max-width: 1000px)')
   return (
     <section id="contact-section" className="contact-section"> 
     <Container className="contact-container" maxW="100%">
       <Heading center={true} title="CONTACT"></Heading>
       <div className="contact-content">
-        <Grid></Grid>
+        <Grid templateColumns={isMinimized ? "1fr " : "1fr 1fr 1fr"} justifyItems="center" gap={1} padding="10px"> </Grid>
       </div>
     </Container>
     </section>
