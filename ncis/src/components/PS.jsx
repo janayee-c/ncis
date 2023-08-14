848c3d6a69482c492962c73193920a66ceb334fc
Janaye Cheong <janaye.c13@gmail.com>
2023-08-14 14:13:42 -0700

    removed slider

--- ncis/src/components/PS.jsx
+++ ncis/src/components/PS.jsx
@@ -1,18 +1,20 @@
+//this is the Products & Services section 
 import React, { useState, useRef } from 'react';
 import '../styles/PS.css';
 import { Heading, ProductPanel, OneProduct } from './export';
 import { Edith, Grace, Hava } from '../images/export';
-import { Container, Grid, useMediaQuery, Spinner } from '@chakra-ui/react';
+import { Container, Grid, useMediaQuery, GridItem, Spinner } from '@chakra-ui/react';
 import { DEVICETEXTS } from '../constants/export';
 import Slider from 'react-slick';
 
 const PS = () => {
-  const [isMinimizedMode] = useMediaQuery('(max-width: 1000px)');
+
 
   const defaultProduct = { name: 'Default Product', desc: DEVICETEXTS.grace }; // Product when nothing is selected
 
   const [activePanelVisible, setActivePanelVisible] = useState(false); //toggles activity off or on 
   const [selectedProduct, setSelectedProduct] = useState(defaultProduct); //sets selectedProduct
+  const [isMinimizedMode] = useMediaQuery('(max-width: 600px)');
 
   const refProducts = useRef([]); //this is an object that stores the references to each product ! 
   const sliderRef = useRef(null);
@@ -30,7 +32,6 @@ const PS = () => {
     autoplay: false,
 
   }
-  
 
   const productPanel = [
     { name: 'HAVA', source: Hava, desc: DEVICETEXTS.hava },
@@ -38,14 +39,27 @@ const PS = () => {
     { name: 'GRACE', source: Grace, desc: DEVICETEXTS.grace },
   ];
 
+  const HAVA = productPanel[0]; // Access the first element
+  const EDITH = productPanel[1]; // Access the second element
+  const GRACE = productPanel[2]; // Access the third element
+
   const handleProductClick = (product) => {
-    setSelectedProduct(product);
-    setActivePanelVisible(true);
+    
+    setSelectedProduct(product); //set the product 
+    setActivePanelVisible(true); //make conditions true 
+
     const productRef = refProducts.current[product.name];
-    if (productRef) {
+    if (productRef && isMinimizedMode) { //use switch here
+
+          const height = 525;
+          setProductHeight(height); 
+
+    }
+    else if (productRef) {
       const height = productRef.offsetHeight;
       setProductHeight(height);
     }
+
     animateProduct(product);
   };
 
@@ -84,25 +98,64 @@ const animateProduct = (product) => {
 
         {isMinimizedMode ? 
 
-        <Slider  {...settings} ref={sliderRef}>
-        {productPanel.map((product, index) => (
-                <button key={index}>
-                  <OneProduct
-                    ref={(element) => {refProducts.current[product.name] = element}} 
-                    name={product.name}
-                    imgSource={product.source}
-                    onClick={() => handleProductClick(product)}
-                    isSelected={selectedProduct === product}
-                    animateStyle={animateProduct(product)}
-                  />
-                </button>
-          ))}
-        </Slider>
-        
+        // MOBILE MODE HERE 
 
-        //forDesktopMode
+            <Grid templateColumns="repeat(3, 1fr)" gap={1}
+            templateRows='repeat(2, 1fr)'
+            alignItems="center" 
+            justifyItems="center">
+
+
+              <GridItem colStart={1} rowStart={2}>
+              <button>
+                <OneProduct
+                ref={(element) => {refProducts.current[EDITH.name] = element}} //assigns 
+                name={EDITH.name}
+                imgSource={EDITH.source}
+                onClick={() => handleProductClick(EDITH)}
+                isSelected={selectedProduct === EDITH}
+                animateStyle={animateProduct(EDITH)}
+                >
+                </OneProduct>
+              </button>
+
+              </GridItem>
+              <GridItem colStart={2} rowStart={1}>
+              <button>
+                <OneProduct
+                ref={(element) => {refProducts.current[HAVA.name] = element}} 
+                name={HAVA.name}
+                imgSource={HAVA.source}
+                onClick={() => handleProductClick(HAVA)}
+                isSelected={selectedProduct === HAVA}
+                animateStyle={animateProduct(HAVA)}
+                >
+                </OneProduct>
+              </button>
+
+              </GridItem>
+
+
+              <GridItem colStart={3} rowStart={2}>
+              <button>
+                <OneProduct
+                ref={(element) => {refProducts.current[GRACE.name] = element}} 
+                name={GRACE.name}
+                imgSource={GRACE.source}
+                onClick={() => handleProductClick(GRACE)}
+                isSelected={selectedProduct === GRACE}
+                animateStyle={animateProduct(GRACE)}
+                >
+                </OneProduct>
+              </button>
+              </GridItem>
+            </Grid>
+
+
+
         
-        : //colon  
+        : // DESKTOP MODE HERE 
+
        
             <Grid templateColumns="repeat(3, 1fr)" gap={1}>
               {productPanel.map((product, index) => (
@@ -114,11 +167,11 @@ const animateProduct = (product) => {
                     onClick={() => handleProductClick(product)}
                     isSelected={selectedProduct === product}
                     animateStyle={animateProduct(product)}
+                    
                   />
                 </button>
               ))}
             </Grid>
-          
 
         }
         </div>
