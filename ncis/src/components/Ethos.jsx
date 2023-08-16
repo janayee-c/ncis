6d6e4497859fd41d59794a40bfb7404fa7f2eecd
Janaye Cheong <janaye.c13@gmail.com>
2023-08-16 14:07:20 -0700

    base functionality

--- ncis/src/components/Ethos.jsx
+++ ncis/src/components/Ethos.jsx
@@ -1,17 +1,22 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import '../styles/Ethos.css';
 
 /* Ethos Components */
 import { Ethical, Capacity, Ocean, Transparency, Information } from '../images/export';
 import { Heading, EthosElement } from '../components/export';
+import { useInView } from 'react-intersection-observer';
 
 import { Container, useMediaQuery } from '@chakra-ui/react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+
 const Ethos = () => {
   const sliderRef = useRef(null);
+  const [visibleProducts, setVisibleProducts] = useState(0);
+
+
   const [isMedium] = useMediaQuery('(max-width: 1200px)');
   const [isSmall] = useMediaQuery('(max-width: 900px)');
 
@@ -42,16 +47,44 @@ const Ethos = () => {
     autoplay: isMedium,
     autoplaySpeed: 2000
   };
+  //observer stuff 
+
+  const { ref, inView } = useInView ({
+    threshold: 1,
+    root: null,
+    rootMargin:"0px"
+  });
+
+  useEffect(() => {
+    if (inView) {
+      const timeout = setTimeout(() => {
+        setVisibleProducts(visibleProducts + 1);
+      }, 250); 
+
+      return () => clearTimeout(timeout); 
+    }
+  }, [inView, visibleProducts]); 
+
+
+  //end of observer stuff 
+
   
   return (
-    <section id="ethos-section" className="ethos-section">
+    <section id="ethos-section" className="ethos-section" ref={ref} inView={inView}>
       <Container className="econtainer" maxW="100%">
         <Heading id="ethos-heading" title="ETHOS" center />
         <div className="ethos-slider-container"> 
           <Slider {...settings} ref={sliderRef}>
             {ethosSources.map((element, index) => (
               <div key={index}>
-                <EthosElement id={index} source={element.source} title={element.title} />
+                <EthosElement 
+                
+                  id={index} 
+                  source={element.source} 
+                  title={element.title} 
+                  startAppear={ visibleProducts > index }>
+                    
+                  </EthosElement>
               </div>
             ))}
           </Slider>
--- ncis/src/components/EthosElement.jsx
+++ ncis/src/components/EthosElement.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "../styles/EthosElement.css";
 
-const EthosElement = ({ source, title, id }) => {
+const EthosElement = ({ source, title, id, startAppear }) => {
   const isEthicalTechnology = id === 0;
 
   return (
     <div className="ethos-element-container">
       <div className={isEthicalTechnology ? "et-content" : "ethos-element-content"}>
-        <img className={isEthicalTechnology ? "et-img" : ""} src={source} alt={title} />
-        <h2 className="caption">{title}</h2>
+        <img className={startAppear ? (isEthicalTechnology ? "et-img" : "") : "invisible"} src={source} alt={title} />
+        <h2 className={startAppear ? "caption" : "invisible"}>{title}</h2>
       </div>
     </div>
   );
