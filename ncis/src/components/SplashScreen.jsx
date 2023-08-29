c74ad9726f82a3f694a3c835b5fedddb25e707d7
Janaye Cheong <janaye.c13@gmail.com>
2023-08-29 15:51:49 -0700

    for checking

--- ncis/src/components/SplashScreen.jsx
+++ ncis/src/components/SplashScreen.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Splash } from '../images/export'
-import '../styles/SplashScreen.css'
-import VideoErrorBoundary from './VideoErrorBoundary'; 
-import VideoPausedError from './VideoPausedError'; 
+import { Splash } from '../images/export';
+import '../styles/SplashScreen.css';
+import VideoErrorBoundary from './VideoErrorBoundary';
+import VideoPausedError from './VideoPausedError';
 
 const SplashScreen = () => {
   const [videoEnded, setVideoEnded] = useState(false);
-
+  const [errorMessage, setErrorMessage] = useState(null); // State for error message
 
   useEffect(() => {
     const videoElement = document.getElementById('splashVideo');
@@ -14,14 +14,13 @@ const SplashScreen = () => {
 
     playPromise
       .then(() => {
-        // Video started playing, check if it's paused
         if (videoElement.paused) {
-          throw new VideoPausedError('Video is paused');
+          setErrorMessage('Video playback started but was immediately paused.');
         }
       })
       .catch(error => {
-        // Handle the error, which could be due to autoplay restrictions
         console.error('Video autoplay error:', error);
+        setErrorMessage('Error while trying to play the video.');
       });
   }, []);
 
@@ -31,23 +30,24 @@ const SplashScreen = () => {
 
   return (
     <VideoErrorBoundary>
-    <div className={`splashscreen ${videoEnded ? 'hidden' : ''}`}>
-      <video
-        id="splashVideo"
-        autoPlay
-        muted
-        onEnded={handleVideoEnded}
-        width="500"
-        height="500"
-        webkit-playsinline 
-        playsInline
-      >
-        <source src={Splash} type="video/mp4" />
-        Your browser does not support videos.
-      </video>
-    </div>
+      <div className={`splashscreen ${videoEnded ? 'hidden' : ''}`}>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <video
+          id="splashVideo"
+          autoPlay
+          muted
+          onEnded={handleVideoEnded}
+          width="500"
+          height="500"
+          webkit-playsinline
+          playsInline
+        >
+          <source src={Splash} type="video/mp4" />
+          Your browser does not support videos.
+        </video>
+      </div>
     </VideoErrorBoundary>
   );
-}
+};
 
 export default SplashScreen;
